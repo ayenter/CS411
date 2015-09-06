e91604b1081c24b82f32f716b2a1cbc129c8833b
ayenter <alecyenter@csu.fullerton.edu>
2015-09-06 09:54:54 -0700

    Assignment 2 base

---
+++ Assignment2/Assignment2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment2
+//
+//  Created by TheDarkKnight on 9/6/15.
+//  Copyright (c) 2015 Alec Yenter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
